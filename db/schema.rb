18f63802032b4b95f9bb47ebbe7c43291626101a
vlsk3104 <vlsk3104@gmail.com>
2021-01-20 15:10:09 +0900

    プロフィール編集機能の実装

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_18_075737) do
+ActiveRecord::Schema.define(version: 2021_01_20_043639) do
 
   create_table "users", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2021_01_18_075737) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "name", null: false
     t.integer "gender", default: 0, null: false
+    t.text "self_introduction"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
