2aca7d4ff174def6e59015062f6ea10de5c63c36
vlsk3104 <vlsk3104@gmail.com>
2021-01-21 21:58:58 +0900

    リアクション機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   devise_for :users,
-   controllers: { registrations: 'registrations', sessions: :sessions } 
+   controllers: {
+     registrations: 'registrations', sessions: :sessions
+    } 
 
   root 'top#index'
 
   resources :users, only: [:show, :index]
+  resources :reactions, only: [:create]
 end
